175cbe782286675364bd02a1b3def95c3779ef61
YingMuo <wl03452329@gmail.com>
2022-01-17 10:20:23 +0000

    Test general_latency

--- general_vs_preempt_rt/run_latency.c
+++ general_vs_preempt_rt/run_latency.c
@@ -8,7 +8,7 @@
 #define MY_PRIORITY (80)
 #define MAX_SAFE_STACK (8*1024)
 #define NSEC_PER_SEC (1000000000)
-#define LOOP_TIME (10000)
+#define LOOP_TIME (1000)
 #define INTERVAL (10000000)
 
 long res[LOOP_TIME];
